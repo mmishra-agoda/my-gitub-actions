f510b8a2360097f17f1591eeac052ed1a6549a0c
mmishra-agoda <67406242+mmishra-agoda@users.noreply.github.com>
2020-11-25 14:57:23 +0700

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -4,6 +4,3 @@ test('mooncake knows what to say', () => {
   expect(mooncakeSays('woooahhh!')).toBe('(o.o) woooahhh!');
 });
 
-test('mooncake is not sure what to say', () => {
-  expect(mooncakeSays()).toBe('(o.o) chookity?');
-});
